26d64b3b4fc72043177beb29801e69f97ffe2d16
Lennart <lennart.schwier@icloud.com>
2020-11-16 12:29:32 +0100

    Init project

---
+++ backend/src/main/java/de/neuefische/saildog/SaildogApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.saildog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaildogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaildogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/de/neuefische/saildog/SaildogApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.saildog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaildogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
