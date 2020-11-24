647aae983e2cac5b7866ad3498a5a8a0fa95cb0a
Lennart <lennart.schwier@icloud.com>
2020-11-24 11:55:25 +0100

    Refactor StormGlassService.js to send jwt with get request

--- frontend/src/service/StormGlassService.js
+++ frontend/src/service/StormGlassService.js
@@ -1,7 +1,13 @@
 import axios from 'axios';
 
 export const getStormGlassWeather = (latitude, longitude) => {
-    const url = `/api/stormglass?latitude=${latitude}&longitude=${longitude}`
-    return axios.get(url)
+    const url = `/api/stormglass?latitude=${latitude}&longitude=${longitude}`;
+    const token = localStorage.getItem("jwtToken");
+    const request = {
+        headers: {
+            "Authorization": "Bearer " + token
+        }
+    }
+    return axios.get(url, request)
         .then(response => response.data);
 }
