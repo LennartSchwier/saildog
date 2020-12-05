1dbfebded3e0c0facfae411e663fb857c39b0146
Lennart <lennart.schwier@icloud.com>
2020-12-05 18:12:06 +0100

    Add ButtonGroupStyles.js

---
+++ frontend/src/commons/ButtonGroupStyles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components/macro";
+
+const ButtonGroupStyles = styled.section`
+position: fixed;
+bottom: 0;
+margin-bottom: var(--size-s);
+display: flex;
+justify-content: space-evenly;
+width: 100vw;
+`
+
+export default ButtonGroupStyles
