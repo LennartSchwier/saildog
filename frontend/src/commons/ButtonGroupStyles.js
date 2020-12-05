a7230542ae30963dce84bed1c0a356f5d2f0eb57
Lennart <lennart.schwier@icloud.com>
2020-12-05 18:52:43 +0100

    Add box shadow

--- frontend/src/commons/ButtonGroupStyles.js
+++ frontend/src/commons/ButtonGroupStyles.js
@@ -3,10 +3,11 @@ import styled from "styled-components/macro";
 const ButtonGroupStyles = styled.section`
 position: fixed;
 bottom: 0;
-margin-bottom: var(--size-s);
+padding: var(--size-s) 0;
 display: flex;
 justify-content: space-evenly;
 width: 100vw;
+box-shadow: 0 -5px 13px -5px grey;
 `
 
 export default ButtonGroupStyles
