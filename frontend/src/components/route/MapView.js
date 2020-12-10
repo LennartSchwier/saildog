3e1f42b21802b1ac821e6202c988b3eff07af340
Lennart <lennart.schwier@icloud.com>
2020-12-10 14:11:28 +0100

    Add info window in map view

--- frontend/src/components/route/MapView.js
+++ frontend/src/components/route/MapView.js
@@ -1,5 +1,5 @@
-import React from "react";
-import {GoogleMap, Marker, Polyline, useLoadScript} from "@react-google-maps/api";
+import React, {useState} from "react";
+import {GoogleMap, InfoWindow, Marker, Polyline, useLoadScript} from "@react-google-maps/api";
 import MapStyles from "../../commons/MapStyles";
 import styled from "styled-components/macro";
 import {IoMdList} from "react-icons/io";
@@ -19,14 +19,13 @@ export default function MapView({ route, toggleView }) {
 
     const startLat = Number(route.legs[0].startWaypoint.latitude);
     const startLng = Number(route.legs[0].startWaypoint.longitude);
+    const [selected, setSelected] = useState(null);
 
     const routing = route.legs.map(leg => (
             {lat: Number(leg.endWaypoint.latitude), lng: Number(leg.endWaypoint.longitude)}
         ));
 
     routing.unshift({lat: startLat, lng: startLng});
-
-    console.log(routing)
 
     const center = {
         lat: startLat,
@@ -57,14 +56,42 @@ export default function MapView({ route, toggleView }) {
                         <Marker
                             key={routing.indexOf(leg)}
                             position={leg}
-                            label={String(routing.indexOf(leg) + 1)}
+                            label={labelMarker(routing, leg)}
+                            onClick={() => {setSelected({leg: leg, index: routing.indexOf(leg)})}}
                         />
                     )}
                     <Polyline path={routing}/>
+                    {selected ?
+                    <InfoWindow position={selected.leg} onCloseClick={() => {setSelected(null)}}>
+                        <div>
+                            <h2>{nameWaypoint(selected?.index)}</h2>
+                            <p>Latitude: {selected?.leg.lat}</p><p>Longitude: {selected?.leg.lng}</p>
+                        </div>
+                    </InfoWindow> : null}
                 </GoogleMap>
                 <button onClick={toggleView}><IoMdList/></button>
             </MapContainer>
         );
+    }
+
+    function labelMarker(collection, item) {
+        if ((collection.indexOf(item)) === 0) {
+            return "Start";
+        }
+        if ((collection.indexOf(item) + 1) === collection.length) {
+            return "End";
+        }
+        return String(collection.indexOf(item));
+    }
+
+    function nameWaypoint(index) {
+        if (index === 0) {
+            return "Start";
+        }
+        if (index + 1 === routing.length) {
+            return "End";
+        }
+        return index + ". Waypoint";
     }
 }
 
