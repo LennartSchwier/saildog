b4363c61fd80578606c94a084c9647fbd3293703
Lennart <lennart.schwier@icloud.com>
2020-12-11 16:56:45 +0100

    Prettier /contexts

--- frontend/src/contexts/RouteContextProvider.js
+++ frontend/src/contexts/RouteContextProvider.js
@@ -1,24 +1,23 @@
-import React, {useEffect, useState} from "react";
-import RouteContext from "./RouteContext";
-import {addNewRoute, getAllRoutesFromUser} from "../service/RouteService";
+import React, { useEffect, useState } from 'react';
+import RouteContext from './RouteContext';
+import { addNewRoute, getAllRoutesFromUser } from '../service/RouteService';
 
 export default function RouteContextProvider({ children }) {
+  const [routes, setRoutes] = useState([]);
 
-    const [routes, setRoutes] = useState([]);
+  const jwtToken = localStorage.getItem('jwtToken');
 
-    const jwtToken = localStorage.getItem('jwtToken');
+  useEffect(() => {
+    jwtToken && getAllRoutesFromUser().then((route) => setRoutes(route));
+  }, [jwtToken]);
 
-    useEffect(() => {
-        jwtToken && getAllRoutesFromUser().then(route => setRoutes(route));
-    }, [jwtToken]);
+  const addNewRouteAndUpdateAllRoutes = (newRoute) => {
+    addNewRoute(newRoute).then((route) => setRoutes([...routes, route]));
+  };
 
-    const addNewRouteAndUpdateAllRoutes = (newRoute) => {
-        addNewRoute(newRoute).then(route => setRoutes([...routes, route]));
-    }
-
-    return (
-        <RouteContext.Provider value={{routes, addNewRouteAndUpdateAllRoutes}}>
-            {children}
-        </RouteContext.Provider>
-    );
-}
+  return (
+    <RouteContext.Provider value={{ routes, addNewRouteAndUpdateAllRoutes }}>
+      {children}
+    </RouteContext.Provider>
+  );
+};
--- frontend/src/contexts/WeatherDataContextProvider.js
+++ frontend/src/contexts/WeatherDataContextProvider.js
@@ -1,45 +1,50 @@
-import React, {useEffect, useState} from "react";
-import WeatherDataContext from "./WeatherDataContext";
-import {getStormGlassWeather} from "../service/StormGlassService";
-import usePositioning from "../hooks/usePositioning";
+import React, { useEffect, useState } from 'react';
+import WeatherDataContext from './WeatherDataContext';
+import { getStormGlassWeather } from '../service/StormGlassService';
+import usePositioning from '../hooks/usePositioning';
 
 export default function WeatherDataContextProvider({ children }) {
+  const [weatherData, setWeatherData] = useState({
+    time: '',
+    airTemperature: 0,
+    waterTemperature: 0,
+    pressure: 0,
+    visibility: 0,
+    currentDirection: 0,
+    currentSpeed: 0,
+    windDirection: 0,
+    windSpeed: 0,
+    waveDirection: 0,
+    waveHeight: 0,
+  });
 
-    const [weatherData, setWeatherData] = useState({
-            time: "",
-            airTemperature: 0,
-            waterTemperature: 0,
-            pressure: 0,
-            visibility: 0,
-            currentDirection: 0,
-            currentSpeed: 0,
-            windDirection: 0,
-            windSpeed: 0,
-            waveDirection: 0,
-            waveHeight: 0,
-    })
+  const [refactoredWeatherData, setRefactoredWeatherData] = useState({
+    windSpeed: 0,
+    waveHeight: 0,
+  });
 
-    const [refactoredWeatherData, setRefactoredWeatherData] = useState({
-        windSpeed: 0,
-        waveHeight: 0
-    });
+  const [latitude, longitude] = usePositioning();
+  const jwtToken = localStorage.getItem('jwtToken');
 
-    const [latitude, longitude] = usePositioning();
-    const jwtToken = localStorage.getItem('jwtToken');
+  useEffect(() => {
+    jwtToken &&
+      latitude &&
+      longitude &&
+      getStormGlassWeather(latitude, longitude).then((data) =>
+        setWeatherData(data)
+      );
+  }, [jwtToken, latitude, longitude]);
 
-    useEffect(() => {
-        jwtToken && latitude && longitude && getStormGlassWeather(latitude, longitude)
-            .then(data => setWeatherData(data))
-    }, [jwtToken, latitude, longitude])
-
-
-
-
-    return (
-        <WeatherDataContext.Provider value={{
-            weatherData, setWeatherData, refactoredWeatherData, setRefactoredWeatherData
-        }}>
-            {children}
-        </WeatherDataContext.Provider>
-    );
+  return (
+    <WeatherDataContext.Provider
+      value={{
+        weatherData,
+        setWeatherData,
+        refactoredWeatherData,
+        setRefactoredWeatherData,
+      }}
+    >
+      {children}
+    </WeatherDataContext.Provider>
+  );
 }
