4bb81d6c00672cbf86e2ff2d2351adc8d9721a3e
Lennart <lennart.schwier@icloud.com>
2020-12-11 16:52:54 +0100

    Prettier /login

--- frontend/src/components/login/Login.test.js
+++ frontend/src/components/login/Login.test.js
@@ -1,24 +1,26 @@
-import {render, screen} from '@testing-library/react';
-import {MemoryRouter as Router} from 'react-router-dom';
-import Login from "./Login";
-import React from "react";
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter as Router } from 'react-router-dom';
+import Login from './Login';
+import React from 'react';
 
 describe('Login Test', () => {
-    test('login with correct credentials directs to dashboard', () => {
-        // GIVEN
-        render(
-            <Router>
-                <Login loginData={{
-                    username: "test",
-                    password: "test"
-                }}/>
-            </Router>
-        );
+  test('login with correct credentials directs to dashboard', () => {
+    // GIVEN
+    render(
+      <Router>
+        <Login
+          loginData={{
+            username: 'test',
+            password: 'test',
+          }}
+        />
+      </Router>
+    );
 
-        // WHEN
-        const usernameInput = screen.getByRole('heading', /log in/i);
+    // WHEN
+    const usernameInput = screen.getByRole('heading', /log in/i);
 
-        // THEN
-        expect(usernameInput).toBeInTheDocument();
-    });
+    // THEN
+    expect(usernameInput).toBeInTheDocument();
+  });
 });
