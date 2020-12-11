9cdfe4b6af3a984bb26d517c2770a18450718091
Lennart <lennart.schwier@icloud.com>
2020-12-11 16:57:39 +0100

    Prettier /hooks

--- frontend/src/hooks/usePositioning.js
+++ frontend/src/hooks/usePositioning.js
@@ -1,27 +1,26 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from 'react';
 
 export default function usePositioning() {
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null);
+  const [errorMessage, setErrorMessage] = useState('');
 
-    const [latitude, setLatitude] = useState(null);
-    const [longitude, setLongitude] = useState(null);
-    const [errorMessage, setErrorMessage] = useState("");
+  const geoSuccess = (position) => {
+    setLatitude(position.coords.latitude);
+    setLongitude(position.coords.longitude);
+  };
 
-    const geoSuccess = (position) => {
-        setLatitude(position.coords.latitude);
-        setLongitude(position.coords.longitude);
-    }
+  const geoError = (error) => {
+    setErrorMessage('Error: Code ' + error.code + ' - ' + error.message);
+  };
 
-    const geoError = (error) => {
-        setErrorMessage("Error: Code " + error.code + " - " + error.message);
-    }
+  useEffect(() => {
+    const geoOptions = {
+      enableHighAccuracy: true,
+      maximumAge: 30000,
+    };
+    navigator.geolocation.watchPosition(geoSuccess, geoError, geoOptions);
+  }, []);
 
-    useEffect(() => {
-        const geoOptions = {
-            enableHighAccuracy: true,
-            maximumAge: 30000
-        }
-        navigator.geolocation.watchPosition(geoSuccess, geoError, geoOptions);
-    }, []);
-
-    return [latitude, longitude, errorMessage];
+  return [latitude, longitude, errorMessage];
 }
