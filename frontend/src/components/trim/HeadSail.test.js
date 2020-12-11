5c98bc4f2a3205ac215adab8de75a475dfcfdbdb
Lennart <lennart.schwier@icloud.com>
2020-12-11 16:55:33 +0100

    Prettier /trim

--- frontend/src/components/trim/HeadSail.test.js
+++ frontend/src/components/trim/HeadSail.test.js
@@ -1,40 +1,38 @@
-import HeadSail from "./HeadSail";
+import HeadSail from './HeadSail';
 import { render, screen } from '@testing-library/react';
-import {MemoryRouter as Router} from 'react-router-dom';
-import React from "react";
-import userEvent from "@testing-library/user-event";
+import { MemoryRouter as Router } from 'react-router-dom';
+import React from 'react';
+import userEvent from '@testing-library/user-event';
 
 describe('component test :: HeadSail', () => {
+  const renderPage = () => {
+    render(
+      <Router>
+        <HeadSail />
+      </Router>
+    );
+  };
 
-    const renderPage = () => {
-        render(
-            <Router>
-                <HeadSail/>
-            </Router>
-        );
-    }
+  xit("renders the HeadSail page and it's content", () => {
+    // GIVEN
+    renderPage();
 
-    xit('renders the HeadSail page and it\'s content', () => {
-        // GIVEN
-        renderPage()
+    // WHEN
+    const headSailHeader = screen.queryByRole('heading', /head sail/i);
 
-        // WHEN
-        const headSailHeader = screen.queryByRole('heading', /head sail/i);
+    // THEN
+    expect(headSailHeader).toBeInTheDocument();
+  });
 
-        // THEN
-        expect(headSailHeader).toBeInTheDocument();
-    });
+  xtest('button "back" redirects to correct page', () => {
+    // GIVEN
+    renderPage();
 
-    xtest('button "back" redirects to correct page', () => {
-        // GIVEN
-        renderPage();
+    // WHEN
+    const backButton = screen.getByRole('button', /back/i);
+    userEvent.click(backButton);
 
-        // WHEN
-        const backButton = screen.getByRole('button', /back/i);
-        userEvent.click(backButton);
-
-
-        // THEN
-        expect(screen.getByRole('heading', /sail trim/i)).toBeInTheDocument();
-    });
+    // THEN
+    expect(screen.getByRole('heading', /sail trim/i)).toBeInTheDocument();
+  });
 });
--- frontend/src/components/trim/MainSail.test.js
+++ frontend/src/components/trim/MainSail.test.js
@@ -1,39 +1,38 @@
-import MainSail from "./MainSail";
+import MainSail from './MainSail';
 import { render, screen } from '@testing-library/react';
-import {MemoryRouter as Router} from "react-router-dom";
-import React from "react";
-import userEvent from "@testing-library/user-event";
+import { MemoryRouter as Router } from 'react-router-dom';
+import React from 'react';
+import userEvent from '@testing-library/user-event';
 
 describe('component test :: MainSail', () => {
+  const renderPage = () => {
+    render(
+      <Router>
+        <MainSail />
+      </Router>
+    );
+  };
 
-    const renderPage = () => {
-        render(
-            <Router>
-                <MainSail/>
-            </Router>
-        );
-    };
+  xit("renders the MainSail page and it's content", () => {
+    // GIVEN
+    renderPage();
 
-    xit('renders the MainSail page and it\'s content', () => {
-        // GIVEN
-        renderPage();
+    // WHEN
+    const mainSailHeader = screen.getByRole('heading', /main sail/i);
 
-        // WHEN
-        const mainSailHeader = screen.getByRole('heading', /main sail/i);
+    // THEN
+    expect(mainSailHeader).toBeInTheDocument();
+  });
 
-        // THEN
-        expect(mainSailHeader).toBeInTheDocument();
-    });
+  xtest('button "back" redirects to correct page', () => {
+    // GIVEN
+    renderPage();
 
-    xtest('button "back" redirects to correct page', () => {
-        // GIVEN
-        renderPage();
+    // WHEN
+    const backButton = screen.getByRole('button', /back/i);
+    userEvent.click(backButton);
 
-        // WHEN
-        const backButton = screen.getByRole('button', /back/i);
-        userEvent.click(backButton);
-
-        // THEN
-        expect(screen.getByRole('heading', /sail trim/i)).toBeInTheDocument();
-    });
+    // THEN
+    expect(screen.getByRole('heading', /sail trim/i)).toBeInTheDocument();
+  });
 });
