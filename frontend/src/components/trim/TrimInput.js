d2cc3e11d9d58c12c81e38aa84931525670768fb
Lennart <lennart.schwier@icloud.com>
2020-12-14 17:11:37 +0100

    Refactor TrimInput.js styling for Safari users

--- frontend/src/components/trim/TrimInput.js
+++ frontend/src/components/trim/TrimInput.js
@@ -19,66 +19,70 @@ export default function TrimInput({ course, setCourse, latitude, longitude }) {
     <PageLayout>
       <Header headerText={'Sail Trim'} />
       <FormStyled>
-        <FieldsetStyled>
-          <legend>Course</legend>
-          <div>
+        <CourseInputStyled>
+          <span>Course</span>
+          <label htmlFor={'closed_hauled'}>
             <input
-              type={'radio'}
-              name={'course'}
-              id={'closed_hauled'}
-              value={'closed_hauled'}
-              checked={course === 'closed_hauled'}
-              onChange={handleRadioButton}
+            type={'radio'}
+            name={'course'}
+            id={'closed_hauled'}
+            value={'closed_hauled'}
+            checked={course === 'closed_hauled'}
+            onChange={handleRadioButton}
             />
-            <label htmlFor={'closed_hauled'}>Closed Hauled</label>
+            Closed Hauled
+          </label>
+          <label htmlFor={'beam_reach'}>
+            <input
+            type={'radio'}
+            name={'course'}
+            id={'beam_reach'}
+            value={'beam_reach'}
+            checked={course === 'beam_reach'}
+            onChange={handleRadioButton}
+            />
+            Beam Reach
+          </label>
+          <label htmlFor={'wind_astern'}>
+            <input
+            type={'radio'}
+            name={'course'}
+            id={'wind_astern'}
+            value={'wind_astern'}
+            checked={course === 'wind_astern'}
+            onChange={handleRadioButton}
+            />
+            Wind Astern
+          </label>
+        </CourseInputStyled>
+        <WeatherInputStyled>
+          <span>Weather</span>
+          <div>
+            <label htmlFor={'windSpeed'}>Wind Speed :</label>
+            <input
+              type={'range'}
+              max={'40'}
+              id={'windSpeed'}
+              name={'windSpeed'}
+              value={refactoredWeatherData.windSpeed}
+              onChange={handleSliderChange}
+            />
+            <output><span>{refactoredWeatherData.windSpeed}</span> knots</output>
           </div>
           <div>
+            <label htmlFor={'waveHeight'}>Wave Height :</label>
             <input
-              type={'radio'}
-              name={'course'}
-              id={'beam_reach'}
-              value={'beam_reach'}
-              checked={course === 'beam_reach'}
-              onChange={handleRadioButton}
+              type={'range'}
+              max={'3'}
+              step={'0.1'}
+              id={'waveHeight'}
+              name={'waveHeight'}
+              value={refactoredWeatherData.waveHeight}
+              onChange={handleSliderChange}
             />
-            <label htmlFor={'beam_reach'}>Beam Reach</label>
+            <output><span>{refactoredWeatherData.waveHeight}</span> meter</output>
           </div>
-          <div>
-            <input
-              type={'radio'}
-              name={'course'}
-              id={'wind_astern'}
-              value={'wind_astern'}
-              checked={course === 'wind_astern'}
-              onChange={handleRadioButton}
-            />
-            <label htmlFor={'wind_astern'}>Wind Astern</label>
-          </div>
-        </FieldsetStyled>
-        <FieldsetStyled>
-          <legend>Weather</legend>
-          <label htmlFor={'windSpeed'}>Wind Speed :</label>
-          <input
-            type={'range'}
-            max={'40'}
-            id={'windSpeed'}
-            name={'windSpeed'}
-            value={refactoredWeatherData.windSpeed}
-            onChange={handleSliderChange}
-          />
-          <output>{refactoredWeatherData.windSpeed} knots</output>
-          <label htmlFor={'waveHeight'}>Wave Height :</label>
-          <input
-            type={'range'}
-            max={'3'}
-            step={'0.1'}
-            id={'waveHeight'}
-            name={'waveHeight'}
-            value={refactoredWeatherData.waveHeight}
-            onChange={handleSliderChange}
-          />
-          <output>{refactoredWeatherData.waveHeight} meter</output>
-        </FieldsetStyled>
+        </WeatherInputStyled>
         <PrimaryButton
           labelButton={'Load weather for current location'}
           handleClick={loadWeather}
@@ -178,22 +182,38 @@ const PageLayout = styled.div`
 const FormStyled = styled.form`
   display: grid;
   grid-template-rows: min-content min-content min-content min-content;
-  row-gap: var(--size-s);
-`;
-
-const FieldsetStyled = styled.fieldset`
-  display: grid;
-  grid-template-rows: 0 1fr 1fr 1fr;
-  row-gap: var(--size-s);
-  margin: var(--size-m);
-  border-radius: var(--size-s);
-  border-color: lightgrey;
-
-  & output,
-  div,
-  legend {
-    font-weight: bold;
+  row-gap: var(--size-xl);
+  
+  label, span {
+  font-weight: bold;
+  }
+  
+  button {
+  outline: none;
+  }
+  
+  button + button {
+  color: darkred;
   }
 `;
+
+const CourseInputStyled = styled.section`
+  display: grid;
+  grid-template-rows: 1.5fr 1fr 1fr 1fr;
+  row-gap: var(--size-s);
+  margin: var(--size-m);
+`;
+
+const WeatherInputStyled = styled.section`
+  display: grid;
+  grid-template-rows: 0.5fr 1fr 1fr;
+  margin: var(--size-m);
+  
+  div {
+  display: grid;
+  row-gap: var(--size-xxs);
+  margin: var(--size-s);
+  }
+`
 
 const ButtonGroup = ButtonGroupStyles;
